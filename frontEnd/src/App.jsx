e28721dbbe0dcffc04036eb843f4d77f9f817e1c
ald2211 <afnadca2@gmail.com>
2024-08-21 17:05:12 +0530

    >Initial frontEnd setup: Created project skeleton,configure tailwind css,remove unwanted elements.

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='absolute top-1/2 left-1/2 text-red-700 text-xl'>
+        working
+      </div>
+    </>
+  )
+}
+
+export default App
