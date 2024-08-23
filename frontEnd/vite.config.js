3047fadd13502cad1de3f6db87d6475ec7c86d26
ald2211 <afnadca2@gmail.com>
2024-08-23 17:25:57 +0530

    >connect env file to front end

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Load .env file from the root folder
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    'process.env': process.env,
+  },
+});
